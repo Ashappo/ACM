544b2b2094dc5cadd4bc48a00642b0b47d77a5d2
Ashappo <a1351901014@gmail.com>
2024-09-22 13:45:02 +0800

    temp

---
+++ Leetcode/leetcode_plugin/leetcode/editor/cn/P2376_CountSpecialIntegers.java
@@ -0,0 +1,72 @@
+//如果一个正整数每一个数位都是 互不相同 的，我们称它是 特殊整数 。 
+//
+// 给你一个 正 整数 n ，请你返回区间 [1, n] 之间特殊整数的数目。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 20
+//输出：19
+//解释：1 到 20 之间所有整数除了 11 以外都是特殊整数。所以总共有 19 个特殊整数。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 5
+//输出：5
+//解释：1 到 5 所有整数都是特殊整数。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 135
+//输出：110
+//解释：从 1 到 135 总共有 110 个整数是特殊整数。
+//不特殊的部分数字为：22 ，114 和 131 。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2 * 10⁹ 
+// 
+//
+// Related Topics数学 | 动态规划 
+//
+// 👍 94, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+
+package leetcode.editor.cn;
+
+/**
+ * 统计特殊整数
+ * @author ambrose
+ * @date 2024-09-20 08:30:36
+ */
+public class P2376_CountSpecialIntegers{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P2376_CountSpecialIntegers().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countSpecialNumbers(int n) {
+		for(int i = 1; i <= n; i++){
+
+		}
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
